3400cc06a32ae00d421acd667f32a09d42d08f7d
SamOutabrae <samuelross06@outlook.com>
2025-05-25 09:39:02 -0400

    Fixed updater script.

--- tracking/updater.py
+++ tracking/updater.py
@@ -3,22 +3,33 @@ import requests
 from datetime import datetime
 import json
 import sys
+import toml
+
+from pathlib import Path
+
+def getKey(path):
+  with open(path + "/config.toml", "r") as f:
+    data = toml.load(f)
+    return data["api_key"].strip()
+  
 
 def main():
   # Have to go up one directory becuase this is in tracking, not root
-  PATH = os.getcwd() + "/.."
-  
-  if(len(sys.argv) > 0):
-      PATH = sys.argv[len(sys.argv) - 1]
+  PATH = str(Path(__file__).parents[1].absolute())
+  key = getKey(PATH)
+
+  players = None
+  with open(PATH + "/data/trackedplayers.txt", "r") as f:
+    players = [player.strip() for player in f.readlines()]
 
   players = open(PATH + "/data/trackedplayers.txt").readlines()
-  PATH = PATH + "/dat/"
+  PATH = PATH + "/data/"
   DATE = datetime.now().strftime("%d-%m-%y")
   
   for player in players:
     player = player.replace("\n", "")
 
-    url = 'https://api.hypixel.net/player?key=e31fa63d-7a49-4b76-ad1e-7c0d312c0a13&uuid=' + player
+    url = f'https://api.hypixel.net/player?key={key}&uuid=' + player
     data = requests.get(url).json()
 
     FILEPATH = PATH + "/trackedplayers/" + player + "/" + DATE + ".json"
@@ -33,7 +44,7 @@ def main():
     os.makedirs(PATH + "trackedplayers/" + player)
     f = open(FILEPATH, "w")
     f.write(json.dumps(data))
-    print("Wrote data for " + player)
+    print(f"Wrote data for {player} at {FILEPATH}.")
     f.close()
     continue
 
